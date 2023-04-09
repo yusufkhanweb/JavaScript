0a78061a9f9d754c9562b6a2718b6293b4462a13
Yusufxon Akbarov <yusufxonakbarov2@gmail.com>
2023-04-10 02:37:25 +0700

    Introduction

---
+++ Github/app.js
@@ -0,0 +1,30 @@
+// repository > project
+// brench > master / main
+
+
+// bir mattalik > laptop bu git account ulash 
+// 1.git config --global user.email "yusufxonakbarov2@gmail.com"
+// 2.git config --global user.name "Yusufxon"
+
+// project un bir martalik > folder bn repo ulash 
+// 1. git init 
+// 2. git remote add orijin https://github.com/yusufkhanweb/Sammi-platforma-darslari
+
+
+// push - vs code > github
+// 1. git add .
+// 2. git commit -m "info"
+// 3. git push origin master
+
+
+// pull 
+// git pull origin master
+
+
+// branch 
+// git branch -> all branch list 
+// git branch navbar --> navbar branch ochib beradi
+// git checkout navbar 
+
+
+
