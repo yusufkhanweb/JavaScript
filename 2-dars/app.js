41223562f3ff3a1e33714aa1ae0a7f42dfe29911
Yusufxon Akbarov <yusufxonakbarov2@gmail.com>
2023-04-11 07:47:40 +0700

    JSga Kirish | Variables | Data Type

---
+++ 2-dars/app.js
@@ -0,0 +1,50 @@
+// console.log("hello javascript separated");
+// let text = " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut repellat dignissimos sequi quas ipsum, vero obcaecati illum in eum iure.";
+// console.log(text);
+
+// let name = "webrean";
+//   console.log("welcome" + name + ", it center");
+//   console.log("welcome  \nto  'Wbbrean'")
+
+
+// ===========================
+// Nul l 
+
+let a;
+let b= null;
+console.log(  a, typeof b);
+  
+// console.log(null == undefined);
+// console.log(null === undefined);
+
+// =========================
+
+// let a;
+// let b= null;
+
+// console.log(null + 1);
+// console.log(undefined + 1);
+// console.log(a +1);
+// console.log(b+1);
+
+// let a = Symbol("text")
+// let b = Symbol("text")
+// console.log(b === a);
+// console.log(b== a);
+
+// Data type nima ??
+// Primimitiv data type lar valeu  qaytaradi.
+// Nan primitiv data type lar referens qaytaradi. yani link.
+
+
+// ==========================
+// Object === array, obj , function
+
+// let ar = ['muhammad ', 12, "iqbol"] //array bln chaqirilayotgan malumotlar index bln chaqiriladi
+// let obj = {name: "webbrain", status: "It senter"}// object bln chqirilayotgan malumotlar key kalis sozi bln chaqiriladi
+// let func = () =>{}
+// function test(){
+//     console.log(ar[1]);
+//     console.log(obj.status);
+// }
+//  test()
